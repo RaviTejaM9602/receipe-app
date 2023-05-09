283374f7c44b4d3409b5301563e70380cc2bb93f
Ravi Teja <ravikanna183@gmail.com>
2023-05-09 22:46:26 +0100

    Update index.js
    Updated Infinite scrolling logic

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 //https://developer.edamam.com/edamam-docs-recipe-api
-const searchForm = document.querySelector('form');
-const inputValue = document.querySelector('form > input')
+const searchForm = document.querySelector('form > button');
+const inputValue = document.querySelector('form > input');
 const searchResultDiv = document.querySelector('.search-result');
 const container = document.querySelector('.container');
 let searchQuery = '';
 const APP_ID = 'fe917a7d';
 const APP_KEY = 'dbbfadc4f4049de4dfb35d6660423d9a';
-let prev = 0;
+let isPageLoad = true;
+let next;
 
 function generateHTML(results) {
     let generatedHTML = ' ';
@@ -22,24 +23,64 @@ function generateHTML(results) {
            <p class="item-data">Cusine Type: ${result?.recipe?.cuisineType[0].charAt(0).toUpperCase()+result?.recipe?.cuisineType[0].slice(1)}</p>
          </div>`
     })
-    return searchResultDiv.innerHTML = generatedHTML;
+    return searchResultDiv.insertAdjacentHTML("beforeend", generatedHTML);
 }
 
 
-searchForm.addEventListener('submit', (e) => {
+searchForm.addEventListener('click', (e) => {
     e.preventDefault();
-    searchQuery = e.target.querySelector('input').value;
-    console.log(searchQuery);
-    fetchReceipe(searchQuery);
-    e.target.querySelector('input').value = '';
+    searchQuery = inputValue.value;
+    const baseURL= `https://api.edamam.com/api/recipes/v2?type=public&q=${searchQuery}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+    loadReceipe(baseURL);
+    inputValue.value = '';
 })
 
-async function fetchReceipe(receipe) {
-    const baseURL= `https://api.edamam.com/api/recipes/v2?type=public&q=${receipe}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+async function fetchReceipe(baseURL) {  
     const response = await fetch(baseURL);
     const data = await response.json();
-    console.log(data)
-    generateHTML(data.hits);
+    return data;
 }
 
 
+const getLastUseEle = () => {
+ return document.querySelector(".search-result > .item:last-child")
+};
+
+const loadReceipe = (url) => {
+  return new Promise((resolve, reject) => {
+    fetchReceipe(url)
+      .then((data) => {
+        data &&
+          data.hits &&
+          generateHTML(data.hits);
+        if (isPageLoad) {
+          next = data?._links?.next?.href;      
+          obseveLastUser();
+          isPageLoad = false;
+        }
+        resolve("Completed Rendering");
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+};
+
+
+const infScrollCallback = (entries, observer) => {
+  const entry = entries[0];
+  if (!entry.isIntersecting) return;
+  loadReceipe(next)
+    .then((resp) => {
+      obseveLastUser();
+    })
+    .catch((error) => console.log(error));
+  observer.unobserve(entry.target);
+};
+
+const infScrollObserver = new IntersectionObserver(infScrollCallback, {});
+
+const obseveLastUser = () => {
+  infScrollObserver.observe(getLastUseEle());
+};
